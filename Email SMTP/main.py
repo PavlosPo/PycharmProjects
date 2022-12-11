5f61a77347c7e83f1a60a94df3d1b02f23111c05
PavlosPo <paulospoulos7@gmail.com>
2022-12-11 14:02:17 +0200

    Update main.py

--- Email SMTP/main.py
+++ Email SMTP/main.py
@@ -1,14 +1,26 @@
-import smtplib
+# import smtplib
+#
+# my_email = "paulospoulos6@gmail.com"
+# password = "xhgyknjsahpyjaqb"
+#
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     # Encrypts the email
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="paulospoulos6@yahoo.com",
+#         msg="Subject:Hello\n\n This is the body of my email"
+#     )
 
-my_email = "paulospoulos6@gmail.com"
-password = "xhgyknjsahpyjaqb"
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    # Encrypts the email
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="paulospoulos6@yahoo.com",
-        msg="Subject:Hello\n\n This is the body of my email"
-    )
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1997 ,month=12, day=15, hour=4)
+print(date_of_birth)
+
