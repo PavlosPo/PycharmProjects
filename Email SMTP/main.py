f5206eae7f632842d3568e2bcc2fb397442eefe0
PavlosPo <paulospoulos7@gmail.com>
2022-12-12 12:17:07 +0200

    Updated to send to more than one email
    Created a function send_email(_)
    where it gets multiple emails to send the same quote.

--- Email SMTP/main.py
+++ Email SMTP/main.py
@@ -5,19 +5,28 @@ import datetime as dt
 import random
 import calendar
 
-my_email = "paulospoulos6@gmail.com"
+emails = ["paulospoulos6@gmail.com", "paulospoulos7@gmail.com"]
 receiver_email = "pavlos.poulos@outlook.com"
 password = "xhgyknjsahpyjaqb"
 now = dt.date.today()
 day_of_week = calendar.day_name[now.weekday()]
 
+
+# It sends the email to every email in list
+def send_email(list_of_emails):
+    sender_email = "paulospoulos6@gmail.com"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=sender_email, password=password)
+        for email in list_of_emails:
+            connection.sendmail(from_addr=receiver_email,
+                                to_addrs=email,
+                                msg="Subject:Motivational Quote\n\n"
+                                    f"Happy {day_of_week}!\n{motivational_quote}")
+
+
+# Main
 with open("quotes.txt", 'r') as quotes:
     lines = quotes.read().splitlines()
     motivational_quote = random.choice(lines)
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs=receiver_email,
-                            msg="Subject:Motivational Quote\n\n"
-                            f"Happy {day_of_week}!\n{motivational_quote}")
+    send_email(emails)
