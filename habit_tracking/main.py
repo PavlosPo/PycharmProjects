9d7b183809fb58bd1016cf1bdb61fd91eab58bc7
PavlosPo <paulospoulos7@gmail.com>
2022-12-14 18:54:00 +0200

    Update main.py
    Added the pixel on pixela graph1

--- habit_tracking/main.py
+++ habit_tracking/main.py
@@ -3,9 +3,9 @@
 import requests
 USERNAME = "pavlosp"
 TOKEN = "hjb32phbobgj"
+GRAPH_ID = "graph1"
 
-
-pixel_endpoint = "https://pixe.la/v1/users"
+pixela_endpoint = "https://pixe.la/v1/users"
 user_parameters = {
     "token": TOKEN,
     "username": USERNAME,
@@ -16,9 +16,9 @@ user_parameters = {
 # response = requests.post(url=pixel_endpoint, json=user_parameters)
 # print(response.text)
 
-graph_endpoint = f"{pixel_endpoint}/{USERNAME}/graphs"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "hours",
     "type": "float",
@@ -29,5 +29,18 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+
+# Creating a pixel
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_config = {
+    "date": "20221214",
+    "quantity": "3.5",
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
 print(response.text)
