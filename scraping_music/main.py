6a7639df9339b83b4e4fd9085a56d61decfdb341
PavlosPo <paulospoulos7@gmail.com>
2022-12-25 15:38:00 +0200

    Update
    It searches for each song that was scrapped from billboard.

--- scraping_music/main.py
+++ scraping_music/main.py
@@ -2,14 +2,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 import os
 
 url = "https://www.billboard.com/charts/hot-100/"
 date = input("Which year to you want to travel to? \nType the date in this format YYYY-MM-DD: ")
 url_end = url + str(date)
 
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 playlist_name = "Top 100 songs "
 description = "Top 100 songs of that time period, based on billboard!"
@@ -19,8 +18,6 @@ response = requests.get(url=url_end).text
 soup = BeautifulSoup(response, "lxml")
 tags = soup.find_all(name="ul",
                      class_="lrv-a-unstyle-list lrv-u-flex lrv-u-height-100p lrv-u-flex-direction-column@mobile-max")
-songs = [tag.find(name="h3", id="title-of-a-story").getText().strip() for tag in tags]
-artists = [tag.find(name="span", class_="c-label").getText().strip() for tag in tags]
 
 # Find the songs in the Spotify to add later
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
@@ -28,7 +25,18 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                redirect_uri="http://example.com",
                                                scope="playlist-modify-private"))
 
-user_id = sp.current_user()['id']
-results = sp.search(q='artist:' + artists[0], type='artist')
-print(results)
+# user_id = sp.current_user()['id']
+# query = f"artist:{artists[0]} track:{songs[0]}"
+# results = sp.search(q=query, type='track')
+# uri_track = results['tracks']['items'][-1]['uri']
 
+songs = [tag.find(name="h3", id="title-of-a-story").getText().strip() for tag in tags]
+artists = [tag.find(name="span", class_="c-label").getText().strip() for tag in tags]
+user_id = sp.current_user()['id']
+uri_track = []
+for index in range(len(songs)):
+    query = f"artist:{artists[index]} track:{songs[index]}"
+    result = sp.search(q=query, type='track')
+    uri_track.append(result['tracks']['items']['uri'])
+
+pprint(uri_track)
