d0d06b9ac316f4711d55c8eb0c73f7fa437b830b
PavlosPo <paulospoulos7@gmail.com>
2022-12-25 12:39:40 +0200

    Creates New project
    It will generate a new playlist on Spotify, based on the input year the user provided. That input year will find the best 100 songs of that time ( source: Billboard.com )

---
+++ scraping_music/main.py
@@ -0,0 +1,7 @@
+
+url = "https://www.billboard.com/charts/hot-100/"
+date = input("Which year to you want to travel to? \nType the date in this format YYYY-MM-DD: ")
+url_end = url + str(date)
+CLIENT_ID = "92fdb96a4f754c86b57cdd7b1cac2fe1"
+CLIENT_SECRET = "34819680ca934d7e8158ea34641f9e45"
+print(url_end)
