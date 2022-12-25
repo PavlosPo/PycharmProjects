e9ccae4dfcbdaeb6bdacdfac654e53e3bb20ca97
PavlosPo <paulospoulos7@gmail.com>
2022-12-25 16:57:14 +0200

    Finalizing
    The app works perfectly.
    Adds in the Spotify all the songs created at top in the Billboard.com.
    Using Spotipy, WebScraping, BeautifulSoup4

--- scraping_music/main.py
+++ scraping_music/main.py
@@ -6,11 +6,12 @@ from pprint import pprint
 import os
 
 url = "https://www.billboard.com/charts/hot-100/"
-date = input("Which year to you want to travel to? \nType the date in this format YYYY-MM-DD: ")
+date = input("Which year do you want to travel to? \nType the date in this format YYYY-MM-DD: ")
+
 url_end = url + str(date)
 
 
-playlist_name = "Top 100 songs "
+playlist_name = f"Billboard Top 100 {date}"
 description = "Top 100 songs of that time period, based on billboard!"
 
 # Find Artists and Song name from Billboard.com
@@ -23,7 +24,9 @@ tags = soup.find_all(name="ul",
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri="http://example.com",
-                                               scope="playlist-modify-private"))
+                                               scope="playlist-modify-public",
+                                               cache_path=".cache",
+                                               show_dialog=True))
 
 # user_id = sp.current_user()['id']
 # query = f"artist:{artists[0]} track:{songs[0]}"
@@ -34,12 +37,18 @@ year = date.split("-")[0]  # Takes the Year
 songs = [tag.find(name="h3", id="title-of-a-story").getText().strip() for tag in tags]
 artists = [tag.find(name="span", class_="c-label").getText().strip() for tag in tags]
 user_id = sp.current_user()['id']
-uri_track = []
+uri_tracks = []
 for index in range(len(songs)):
     query = f"track:{songs[index]} year:{year}"
     try:
         result = sp.search(q=query, type='track')
-        uri_track.append(result['tracks']['items'][0]['uri'])
+        uri_tracks.append(result['tracks']['items'][0]['uri'])
     except:
         print(f"*********\nSong: {songs[index]}\nArtist: {artists[index]}\nDoesn't Exists, Skipped.\n*********")
-pprint(uri_track)
+
+
+# Create the playlist to the user
+playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=True, description=description)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=uri_tracks)
+
