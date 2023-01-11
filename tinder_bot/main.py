32ceac5e410dbd2bf36e4475e2f1df2918b4d31e
PavlosPo <paulospoulos7@gmail.com>
2023-01-11 13:45:04 +0200

    Update main.py

--- tinder_bot/main.py
+++ tinder_bot/main.py
@@ -12,10 +12,16 @@ time.sleep(3)
 login_in_button = driver.find_elements(by=By.CSS_SELECTOR, value='.w1u9t036 .l17p5q9z')[2]
 login_in_button.click()
 
-facebook_login = WebDriverWait(driver, timeout=5).until(lambda driver: driver.find_element(by=By.XPATH, value='//*[@id="o793001744"]/main/div/div[1]/div/div/div[3]/span'))
 
-# print([item.text for item in facebook_login])
-# print(len(facebook_login))
-# dummy_elements[0].click()
-print(facebook_login.text)
+# Phone Number Section
+phone_number_login = WebDriverWait(driver, timeout=5).until(lambda driver: driver.find_element(by=By.XPATH, value='//*[@id="o793001744"]/main/div/div[1]/div/div/div[3]/span/div[3]/button/div[2]/div[2]'))
+phone_number_login.click()
+type_phone_number = WebDriverWait(driver, timeout=10).until(lambda driver: driver.find_element(by=By.NAME, value="phone_number"))
+type_phone_number.send_keys("6940820115")
+complete_phone_number_button = WebDriverWait(driver, timeout=600).until(lambda driver: driver.find_element(by=By.XPATH, value='//*[@id="o793001744"]/main/div/div[1]/div/button/div[2]'))
+complete_phone_number_button.click()
+
+like_button = WebDriverWait(driver, timeout=10).until(lambda driver: driver.find_element(by=By.XPATH, value='//*[@id="o-1773584476"]/div/div[1]/div/div/main/div/div/div[1]/div/div[4]/div/div[4]/button/span/span'))
+like_button.click()
 time.sleep(3)
+print("Clicked")
