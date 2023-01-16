1c6c426bde82947521f4d46924548b59ab6c6dfb
PavlosPo <paulospoulos7@gmail.com>
2023-01-16 19:33:48 +0200

    Created new Code
    Created code that interacts with Flask and HTML files

---
+++ my-personal-site/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # The name of current Directory, so it can RUN!!!
+
+
+@app.route("/")
+def home():
+    return render_template("pavlos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
