bdc6b20d407d35c42953348cef4613ae4d63f994
PavlosPo <paulospoulos7@gmail.com>
2023-01-17 17:21:40 +0200

    Created css and more using flask

--- my-personal-site/server.py
+++ my-personal-site/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)  # The name of current Directory, so it can RUN!!!
 
 @app.route("/")
 def home():
-    return render_template("pavlos.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
