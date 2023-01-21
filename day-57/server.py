2ea823337cd2ecdead60a24fc098a80045bf90fe
PavlosPo <paulospoulos7@gmail.com>
2023-01-21 12:39:05 +0200

    Created a small guess game

--- day-57/server.py
+++ day-57/server.py
@@ -44,11 +44,20 @@ def home():
 @app.route("/guess/<name>")
 def guess(name):
     [gender, age] = api_brain(name)
-    print(gender, age)
-    return render_template("index.html",
+    return render_template("guess.html",
                            name=name.capitalize(),
                            gender=gender.capitalize(),
                            age=age)
+
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    return render_template("blog.html",
+                           posts=all_posts)
 
 
 if __name__ == "__main__":
